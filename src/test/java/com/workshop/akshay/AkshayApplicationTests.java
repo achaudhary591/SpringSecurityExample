a0b9bf02e57651727fd54c430612c0f5fa211f7e
achaudhary591 <achaudhary591@gmail.com>
2022-12-15 23:54:22 +0530

    initial commit

---
+++ src/test/java/com/workshop/akshay/AkshayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.akshay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkshayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
